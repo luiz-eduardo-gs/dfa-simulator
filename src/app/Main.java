9b7fe6d75390ce43aebe30f76f44b38e12af6acb
luizeduardoufg <luizeduardo.ufg@gmail.com>
2019-10-12 15:02:31 -0300

    DFA concluido

--- src/app/Main.java
+++ src/app/Main.java
@@ -10,11 +10,18 @@ public class Main
         DfaSimulator dfa = new DfaSimulator();
         
         /*Selecionando o arquivo contento a tabela de transicao e imprimindo o arquivo*/
-        File tableFile = dfa.selectFile();
+        File tableFile = dfa.selectFile("Table File");
         String[] tableFields = dfa.readFile(tableFile);
+        dfa.organizeVariables(tableFields);
+        
+//        System.out.println(dfa.getTransitions("q3").get(1));
         
         /*Selecionando o arquivo contento as strings teste e imprimindo o arquivo*/
-        File stringsFile = dfa.selectFile();
+        File stringsFile = dfa.selectFile("Strings File");
         String[] stringsFields = dfa.readFile(stringsFile);
+        
+        
+        dfa.organizeInputs(stringsFields);
+        dfa.validate();
     }
 }
--- src/dfa/DfaSimulator.java
+++ src/dfa/DfaSimulator.java
@@ -8,14 +8,116 @@ import java.io.IOException;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileSystemView;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DfaSimulator
 {
+    private String[] alphabet;
+    private String currentState;
+    private final ArrayList<String> inputs = new ArrayList<>();
+    private String initialState;
+    private String[] finalStates;
+    private final ArrayList<String> states = new ArrayList();
+    private final Map<String, List<String>> transitions = new HashMap<>();
+    
+    /*FAz com que as strings lidas como entrada sejam organizadas em uma lista*/
+    public void organizeInputs(String[] inputs)
+    {
+        this.inputs.addAll(Arrays.asList(inputs));
+    }
+    
+    
+    /*Valida se o automato aceita a string ou nao*/
+    public void validate()
+    {
+        for(String test : inputs)
+        {
+            currentState = getInitialState();
+            System.out.println(currentState);
+            for(String c : test.split(","))
+            {
+                currentState = transitions.get(currentState).get(Integer.parseInt(c));
+                System.out.println(currentState);     
+            }
+            /*Valida se o automato aceita a string teste*/
+            for(String finalState : getFinalStates())
+                if(currentState.equals(finalState))
+                    System.out.println("ACEITA");
+                else
+                    System.out.println("NAO ACEITA");
+        }
+    }
+    
+    /*Associa estados e transicoes a variaveis*/
+    public void organizeVariables(String[] tableString)
+    {
+        /*Coloca o alfabeto do automato em inputSimbols*/
+        setAlphabet(tableString[0].split(","));
+        
+        /*Coloca o estado inicial do automato em initialState*/
+        setInitialState(tableString[1].replace(">", ""));
+        
+        /*Coloca os estados finais do automato em finalStates*/
+        setFinalStates(tableString[2].replace("*", "").split(","));
+        
+        /*Coloca os estados do automato na lista states*/
+        String[] tableStringAux = tableString;
+        tableStringAux[0] = null;
+        tableStringAux[1] = null;
+        tableStringAux[2] = null;
+        
+        for(String obj : tableStringAux)
+        {
+            if(obj != null)
+                states.add(obj.split(":")[0]);
+        }
+        
+        /*Coloca as transicoes do automato no map transitions*/
+        tableStringAux = tableString;
+        tableStringAux[0] = null;
+        tableStringAux[1] = null;
+        tableStringAux[2] = null;
+        
+        ArrayList<String> auxList = new ArrayList<>();
+        
+        for (String str : tableStringAux) {
+            if (str != null) {
+                auxList.add(str.split(":")[1]);
+            }
+        }
+        
+        for(int i=0;i<states.size();i++)
+        {
+            transitions.put(states.get(i), Arrays.asList(auxList.get(i).split(",")));
+        }
+        
+//        /*Imprime cada atributo da classe*/
+//        for(String obj : getInputSimbols()){
+//            System.out.printf("Alphabet:" + obj + "\n");
+//        }
+//        
+//        System.out.println("Initial State: " + getInitialState());
+//        
+//        for(String obj : getFinalStates()){
+//            System.out.printf("Final states: " + obj + "\n");
+//        }
+//        for(String obj : states){
+//            System.out.printf("States: " + obj + "\n");
+//        }
+//        
+//        for(String key : transitions.keySet())
+//            System.out.println(transitions.get(key));
+    }
     
     /*Seleciona o arquivo de texto atraves de um JFileChooser*/
-    public File selectFile()
+    public File selectFile(String dialogTitle)
     {
         JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        jfc.setDialogTitle("Open " + dialogTitle);
         File selectedFile = null;
         int returnValue = jfc.showOpenDialog(null);
         if(returnValue == JFileChooser.APPROVE_OPTION)
@@ -61,11 +163,50 @@ public class DfaSimulator
 
         /*Organiza o arquivo em variavel*/
         String[] fields = content.toString().split(System.lineSeparator());
-        for(String linha : fields)
-        {
-            System.out.println(linha);
-        }
+//        for(String linha : fields)
+//        {
+//            System.out.println(linha);
+//        }
         
         return fields;
-    }    
+    }
+    
+    public List<String> getTransitions(String key)
+    {
+        return transitions.get(key);
+    }
+    
+    public String[] getAlphabet() {
+        return alphabet;
+    }
+
+    public void setAlphabet(String[] alphabet) {
+        this.alphabet = alphabet;
+    }
+
+    public String getInitialState() {
+        return initialState;
+    }
+
+    public void setInitialState(String initialState) {
+        this.initialState = initialState;
+    }
+
+    public String[] getFinalStates() {
+        return finalStates;
+    }
+
+    public void setFinalStates(String[] finalStates) {
+        this.finalStates = finalStates;
+    }
+
+    public String getCurrentState() {
+        return currentState;
+    }
+
+    public void setCurrentState(String currentState) {
+        this.currentState = currentState;
+    }
+    
+    
 }
